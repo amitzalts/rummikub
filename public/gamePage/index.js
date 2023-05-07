8a226905c4b2a662760f5ab707894147d1c2a47b
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-05-07 22:19:32 +0300

    game board WIP

--- public/gamePage/index.js
+++ public/gamePage/index.js
@@ -25,9 +25,13 @@ function createBoard() {
 }
 gridArray[67].classList.add("red");
 gridArray[67].textContent = "7";
-gridArray[68].classList.add("black");
+gridArray[68].classList.add("green");
 gridArray[68].textContent = "7";
 gridArray[69].classList.add("gold");
 gridArray[69].textContent = "7";
 gridArray[70].classList.add("blue");
 gridArray[70].textContent = "7";
+const playerTiles = document.querySelectorAll(".tile");
+playerTiles.forEach((tile) => tile.addEventListener("click", () => {
+    tile.classList.add("active");
+}));
--- public/gamePage/index.ts
+++ public/gamePage/index.ts
@@ -29,9 +29,19 @@ function createBoard() {
 
 gridArray[67].classList.add("red");
 gridArray[67].textContent = "7";
-gridArray[68].classList.add("black");
+gridArray[68].classList.add("green");
 gridArray[68].textContent = "7";
 gridArray[69].classList.add("gold");
 gridArray[69].textContent = "7";
 gridArray[70].classList.add("blue");
 gridArray[70].textContent = "7";
+
+const playerTiles = document.querySelectorAll(
+  ".tile"
+) as NodeListOf<HTMLDivElement>;
+
+playerTiles.forEach((tile) =>
+  tile.addEventListener("click", () => {
+    tile.classList.add("active");
+  })
+);
